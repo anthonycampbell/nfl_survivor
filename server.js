178475dec7ed2afaaa07a0ece04ccd50e9dbcdda
anthonycampbell <anthony-campbell@hotmail.com>
2021-11-14 04:17:15 -0800

    better folder structure for deploying to heroku

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(cors());
 // API
 const users = require('./api/users');
 app.use('/api/users', users);
-app.use(express.static(path.join(__dirname, '../build')))
+app.use(express.static(path.join(__dirname, "client", "build")))
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
 })
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
